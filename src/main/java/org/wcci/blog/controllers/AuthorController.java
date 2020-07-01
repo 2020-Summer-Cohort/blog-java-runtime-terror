be43f6e4ddaa4b30d41c901ba8dd5f7045deaa82
Derek Mosher <derekmosher21@gmail.com>
2020-07-01 17:26:59 -0400

    fixed controllers

--- src/main/java/org/wcci/blog/controllers/AuthorController.java
+++ src/main/java/org/wcci/blog/controllers/AuthorController.java
@@ -2,7 +2,11 @@ package org.wcci.blog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.wcci.blog.entities.Author;
 import org.wcci.blog.storage.AuthorStorage;
 
 @Controller
@@ -13,6 +17,25 @@ public class AuthorController {
         this.authorStorage = authorStorage;
     }
 
+    @GetMapping("all-authors")
+    public String showAllAuthors(Model model) {
+        model.addAttribute("authors", authorStorage.findAllAuthors());
+        return "all-authors-template";
+    }
 
+    @GetMapping("author/{authorName}")
+    public String showSingleAuthor(@PathVariable String authorName, Model model) {
+        model.addAttribute("author", authorStorage.findAuthorByAuthorName(authorName));
+        return "single-author-template";
+
+    }
+
+    @PostMapping("authors/add")
+    public String addAuthor(String authorName) {
+        Author authorToAdd = new Author(authorName);
+        authorStorage.addAuthor(authorToAdd);
+        return "all-authors-template";
+
+    }
 
 }
---
+++ src/main/java/org/wcci/blog/controllers/TagController.java
@@ -0,0 +1,37 @@
+package org.wcci.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.wcci.blog.entities.Tag;
+import org.wcci.blog.storage.TagStorage;
+
+@Controller
+public class TagController {
+    TagStorage tagStorage;
+
+    public TagController(TagStorage tagStorage) {
+        this.tagStorage = tagStorage;
+    }
+
+    @GetMapping ("all-tags")
+    public String showAllTags (Model model) {
+        model.addAttribute("tags", tagStorage.findAllTags());
+        return "all-hashtags-template";
+    }
+
+    @GetMapping ("tag/{tagName}")
+    public String showSingleTag (@PathVariable String tagName, Model model ) {
+        model.addAttribute("tag", tagStorage.findTagByTagName(tagName));
+        return "single-hashtag-template";
+    }
+
+    @PostMapping ("tag/add")
+    public String addTag (String tagName) {
+        Tag tagToAdd = new Tag(tagName);
+        tagStorage.addTag (tagToAdd);
+        return "all-hashtags-template";
+    }
+}
